19daa39a4f6c52369e66fb6746f2067a02fb8ac8
FiilipeQuerino <flpqrn@gmail.com>
2022-09-23 12:49:54 -0300

    Intalado dependecias.

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const mysql=require('mysql');
+const handlebars=require('express-handlebars');
+const app=express();
+
+//Start Server
+app.listen(3000,function(req, res){
+    console.log('Servidor está rodando!!');
+});
